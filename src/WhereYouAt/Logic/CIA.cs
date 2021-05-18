82a804177845af1ea9484831d3b9b7b92fb5c8a8
Berkan <BerkanYildiz@users.noreply.github.com>
2021-05-18 04:17:33 +0200

    Comma to point

--- src/WhereYouAt/Logic/CIA.cs
+++ src/WhereYouAt/Logic/CIA.cs
@@ -17,17 +17,18 @@
             GeoWatcher.PositionChanged += (Sender, Args) =>
             {
                 var CurrentLocation = Args.Position.Location;
-                var Latitude = CurrentLocation.Latitude.ToString("0.0000000000");
-                var Longitude = CurrentLocation.Longitude.ToString("0.0000000000");
+                var Latitude = CurrentLocation.Latitude.ToString("0.0000000000").Replace(',', '.');
+                var Longitude = CurrentLocation.Longitude.ToString("0.0000000000").Replace(',', '.');
                 var HorizontalAccuracy = CurrentLocation.HorizontalAccuracy.ToString("0.0000000000");
                 var VerticalAccuracy = CurrentLocation.VerticalAccuracy.ToString("0.0000000000");
 
-                Console.WriteLine($"[*] Located at {Latitude} / {Longitude}. (Accuracy: {HorizontalAccuracy}m/{VerticalAccuracy}m)");
+                Console.WriteLine($"[*] Located at '{Latitude}, {Longitude}'. (Accuracy: {HorizontalAccuracy}m/{VerticalAccuracy}m)");
             };
 
             GeoWatcher.StatusChanged += (Sender, Args) =>
             {
-                if (Args.Status == GeoPositionStatus.Initializing)
+                if (Args.Status == GeoPositionStatus.Initializing ||
+                    Args.Status == GeoPositionStatus.NoData)
                 {
                     return;
                 }
@@ -39,7 +40,7 @@
             // Start to locate the computer.
             // 
 
-            if (!GeoWatcher.TryStart(false, TimeSpan.FromSeconds(10)))
+            if (!GeoWatcher.TryStart(false, TimeSpan.FromSeconds(40)))
             {
                 Console.WriteLine("[*] Failed to start the geolocation.");
                 return;
